a76d75ee48f9a0cb6555654a5a545d813ced51a0
Pierre Denissen <pierre.denissen@gmail.com>
2017-03-31 20:54:21 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# magento2-really-blank-boilerplate
---
+++ app/i18n/Blank-default/nl_NL/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+	'Blank-default_nl_nl',
+	__DIR__
+);
